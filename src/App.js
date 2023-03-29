2a03fba1848c8c0838bf609a681f3013054d1b1a
Keitha Noel-Louis <knoellouis@gmail.com>
2023-03-29 22:10:42 +0200

    second commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import qrcode from "./image-qr-code.png";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app-container">
+        <img src={qrcode} className="qr-code" alt="logo" />
+        <div className="text-container">
+          <h3>Improve your front-end skills by building projects</h3>
+          <p>
+            Scan the QR code to visit Frontend Mentor and take your coding
+            skills to the next level
+          </p>
+        </div>
+      </div>
+      <div class="attribution">
+        Challenge by 
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+          rel="noreferrer"
+        >
+           Frontend Mentor
+        </a>
+        . Coded by{" "}
+        <a
+          href="https://www.keithanoellouis.com"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Keitha Noel-Louis
+        </a>
+        .
+      </div>
+    </div>
+  );
+}
+
+export default App;
