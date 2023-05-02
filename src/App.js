5f4b78c134819ba1282c7cdd9b30c3922976116a
Keitha Noel-Louis <knoellouis@gmail.com>
2023-05-02 15:06:51 +0200

    move attribution to footer in index.html

--- src/App.js
+++ src/App.js
@@ -7,25 +7,6 @@ function App() {
       <div className="app-container">
         <Card />
       </div>
-      <div className="attribution">
-        Challenge by
-        <a
-          href="https://www.frontendmentor.io?ref=challenge"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <span>Frontend Mentor</span>
-        </a>
-        . Coded by{" "}
-        <a
-          href="https://www.keithanoellouis.com"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <span>Keitha Noel-Louis</span>
-        </a>
-        .
-      </div>
     </div>
   );
 }
